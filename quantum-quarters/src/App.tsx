9fdc02a0ebc8c2e6a26adf082a5720e9411e38d5
Marruc74 <d.andersson.1974@gmail.com>
2025-06-23 20:14:15 +0200

    Teseting Tailwind

--- quantum-quarters/src/App.tsx
+++ quantum-quarters/src/App.tsx
@@ -10,21 +10,37 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 
 //core
 import "primereact/resources/primereact.min.css";
+import { useState } from "react";
 
 function App() {
   const routes = getRoutes();
+  const [theme, setTheme] = useState("default");
+
+  // Determine the theme class
+  const themeClass = theme === "theme-dark" ? "theme-dark" : "";
 
   return (
     <Provider store={store}>
       <Header />
-      <div className="card2">
-        <BrowserRouter>
-          <Routes>
-            {routes.map(({ path, element }, index) => (
-              <Route key={index} path={path} element={element} />
-            ))}
-          </Routes>
-        </BrowserRouter>
+      <div>
+        <select
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+          style={{ margin: "16px" }}
+        >
+          <option value="default">Default</option>
+          <option value="theme-dark">Dark</option>
+        </select>
+        Theme: {themeClass}
+        <div className={`card2 ${themeClass}`}>
+          <BrowserRouter>
+            <Routes>
+              {routes.map(({ path, element }, index) => (
+                <Route key={index} path={path} element={element} />
+              ))}
+            </Routes>
+          </BrowserRouter>
+        </div>
       </div>
     </Provider>
   );
